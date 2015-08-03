8c918462150a1e2f4bb953d9467128f31628a940
LCAir4 <lcair4@172-17-6-58.DYNAPOOL.NYU.EDU>
2015-08-03 15:58:58 -0400

    New Wirefram w/ colorful bridge

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-$('#p-1').hide();
-$('#p-2').hide();
-$('#p-3').hide();
+$('p').hide();
 
-$('#heading-1').click(function){
-	$('#p-1').show();
-	$('#p-2').hide();
-	$('#p-3').hide();
-});
-
-$('#heading-2').click(function){
-	$('#p-1').hide();
-	$('#p-2').show();
-	$('#p-3').hide();
-});
-
-$('#heading-3').click(function){
-	$('#p-1').hide();
-	$('#p-2').hide();
-	$('#p-3').show();
+$('#modal').click(function(){
+	$('#p-1').toggle();
+	
 });
